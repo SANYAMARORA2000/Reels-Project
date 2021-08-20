a13de537651fb2ee375a3861f851186f3c8f9c9c
SANYAMARORA2000 <80115103+SANYAMARORA2000@users.noreply.github.com>
2021-08-21 00:56:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {
 import Header from "./Components/Header";
 import Feeds from "./Components/Feeds";
 import Login from "./Components/Login";
-import Profile from "./Components/Profile";
+
 import Signup from "./Components/Signup";
 import { AuthContext, AuthProvider } from "./context/AuthProvider";
 
@@ -23,7 +23,7 @@ function App() {
             <Route path="/login" component={Login} exact></Route>
             <Route path="/signup" component={Signup} exact></Route>
             <PrivateRoute path="/" comp={Feeds}></PrivateRoute>
-            <PrivateRoute path="/profile" comp={Profile}></PrivateRoute>
+           
           </Switch>
         </div>
       </Router>
